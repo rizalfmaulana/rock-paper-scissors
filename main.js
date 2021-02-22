a597c6800d8dfb2f7cd4c41d5ccac326f8627b6c
Rizal Maulana <rizalfmaulana@gmail.com>
2021-02-22 10:03:34 +0700

    add game pattern

--- main.js
+++ main.js
@@ -17,11 +17,62 @@ function convert(word) {
   return '<i class="far fa-hand-scissors"></i>';
 }
 
-const game = (userChoice) => {
+function game(userChoice) {
   var box = document.getElementById("challenge");
   box.style.display = "inline-flex";
   var userDiv = document.getElementById("yourObject");
   userDiv.innerHTML = convert(userChoice);
   var comDiv = document.getElementById("comObject");
   comDiv.innerHTML = convert(comChoice);
+  if (
+    (userChoice === "paper" && comChoice === "rock") ||
+    (userChoice === "rock" && comChoice === "scissors") ||
+    (userChoice === "scissors" && comChoice === "paper")
+  ) {
+    win(userChoice);
+  } else if (userChoice === comChoice) {
+    draw(userChoice);
+  } else {
+    lose(userChoice);
+  }
+  function continueGame() {
+    i = Math.floor(Math.random() * 3);
+    comChoice = choices[i];
+    box.style.display = "none";
+  }
+  setTimeout(continueGame, 1200);
+}
+
+const win = (btn) => {
+  userPoints++;
+  document.getElementById("who").innerHTML = "You Win!";
+  var btn = document.getElementById(btn);
+  btn.classList.remove("btn");
+  btn.classList.add("green");
+  setTimeout(() => {
+    btn.classList.add("btn");
+    btn.classList.remove("green");
+  }, 1200);
+};
+
+const draw = (btn) => {
+  document.getElementById("who").innerHTML = "Draw";
+  var btn = document.getElementById(btn);
+  btn.classList.remove("btn");
+  btn.classList.add("gray");
+  setTimeout(() => {
+    btn.classList.add("btn");
+    btn.classList.remove("gray");
+  }, 1200);
+};
+const lose = (btn) => {
+  comPoints++;
+  document.getElementById("who").innerHTML = "You Lose";
+  var btn = document.getElementById(btn);
+  btn.classList.remove("btn");
+  btn.classList.add("red");
+  setTimeout(() => {
+    btn.classList.add("btn");
+    btn.classList.remove("red");
+  }, 1200);
 };
